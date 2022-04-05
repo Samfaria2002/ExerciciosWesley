63d81a82d5e9b4e7b2026aea163335d6b3e4a63f
Samuel Faria <samuel.faria2002@hotmail.com>
2022-04-05 20:59:17 -0300

    1° commit: Lista 5

---
+++ exercicio3.java
@@ -0,0 +1,17 @@
+public class exercicio3 {
+    /*
+     * Realize uma pesquisa de audiência de canal de TV em várias casas de uma
+     * cidade em um
+     * determinado dia. Para cada casa consultada, devem ser fornecidos: o número do
+     * canal
+     * (4, 5, 7, 12) e o número de pessoas que estavam assistindo àquele canal. O
+     * algoritmo
+     * deve:
+     * a. Ler um número indeterminado de dados: número do canal e número de
+     * pessoas assistindo o canal.
+     * b. Calcular e apresentar o percentual de audiência de cada canal.
+     * c. Encerrar o processamento quando o usuário informar o número do canal
+     * igual a ZERO.
+     */
+
+}
