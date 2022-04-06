30f23cb69ed0d5c66f751dcd3bf93f5e016b1c25
Samuel Faria <samuel.faria2002@hotmail.com>
2022-04-05 21:42:34 -0300

    4° commit: Lista5/Exercicio3  Wesley

--- exercicio3.java
+++ exercicio3.java
@@ -22,23 +22,34 @@ public class exercicio3 {
         int canal;
         int[] canais = { 4, 5, 7, 12 };
         int c4 = 0, c5 = 0, c7 = 0, c12 = 0;
+        int p4 = 0, p5 = 0, p7 = 0, p12 = 0;
+        int p4soma = 0, p5soma = 0, p7soma = 0, p12soma = 0;
         int pessoas;
         int pessoasSoma = 0;
 
         do {
             System.out.println("Entre com o número do canal: ");
             canal = input.nextInt();
-            if (canal == 4) {
-                c4++;
-            } else if (canal == 5) {
-                c5++;
-            } else if (canal == 7) {
-                c7++;
-            } else if (canal == 12) {
-                c12++;
-            }
             System.out.println("Quantas pessoas estavam assistindo a esse canal?");
             pessoas = input.nextInt();
+
+            if (canal == 4) {
+                c4++;
+                p4 = pessoas;
+                p4soma = p4soma + p4;
+            } else if (canal == 5) {
+                c5++;
+                p5 = pessoas;
+                p5soma = p5soma + p5;
+            } else if (canal == 7) {
+                c7++;
+                p7 = pessoas;
+                p7soma = p7soma + p7;
+            } else if (canal == 12) {
+                c12++;
+                p12 = pessoas;
+                p12soma = p12soma + p12;
+            }
 
             pessoasSoma = pessoas + pessoasSoma;
 
